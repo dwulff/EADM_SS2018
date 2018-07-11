b615dd470ba668812eef660ba66dffea9f3d942c
Dirk Wulff <dirk.wulff@gmail.com>
2018-07-11 11:44:30 +0200

    efw

---
+++ my_first_mousetracking_analysis/my_first_save.R
@@ -0,0 +1,30 @@
+install.packages("mousetrap")
+library(mousetrap)
+
+
+?mt_import_mousetrap
+
+
+mousetrap
+some_vector
+
+some_vector <- c(1, 2, 3)
+some_vector
+print(some_vector)
+
+some_vector + 2
+
+some_vector
+
+some_vector <- some_vector + 2
+
+some_vector
+
+some_vector <- mean(some_vector)
+some_vector <- 'hello world'
+
+
+
+
+
+
